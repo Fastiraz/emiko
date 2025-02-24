08b22c101a4ab1cd0c43c873f9b7553302b6d39a
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2025-02-24 23:05:49 +0100

    fix: v0.0.1-pre-alpha
    fix path to log file.

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,21 @@ struct Config {
 }
 
 fn log(level: &str, message: &str) {
+  let home_directory = std::env::var("HOME")
+    .or_else(|_| std::env::var("USERPROFILE"))
+    .unwrap_or_else(|_| "Unable to get your home dir!".to_string());
+
+  let mut log_path = std::path::PathBuf::new();
+  log_path.push(home_directory);
+  log_path.push(".config");
+  log_path.push("emiko");
+  log_path.push("emiko");
+  log_path.set_extension("log");
+
   let mut file = std::fs::OpenOptions::new()
     .create(true)
     .append(true)
-    .open("file.log")
+    .open(log_path)
     .expect("Failed to open log file");
 
   let timestamp = std::time::SystemTime::now()
